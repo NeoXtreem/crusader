9dd46fb542c67c5f4b89624db5e1d1c49838443f
Jason Payne <github@x-treem.net>
2019-10-07 00:39:26 +0200

    Move database settings from CAPE API to ML API

--- Xtreem.Crusader.Cape.Api/Controllers/CapeController.cs
+++ Xtreem.Crusader.Cape.Api/Controllers/CapeController.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Xtreem.Crusader.Cape.Api.Services.Interfaces;
 using Xtreem.Crusader.Data.Models;
@@ -33,32 +32,6 @@ namespace Xtreem.Crusader.Cape.Api.Controllers
             var ohlcvPrediction = _predictionService.Predict(_ohlcvMappingService.Map(ohlcv));
 
             return Ok(ohlcvPrediction.ClosePrediction);
-        }
-
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
